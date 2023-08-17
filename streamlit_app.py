07e3c45814ed9857a5939b734ba12965fe426a04
Ben <101144776+benlode@users.noreply.github.com>
2023-08-17 12:05:55 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 import numpy as np
 
 with st.chat_message("assistant", avatar="😄"):
-    st.write("Hello 👋 ")
+    st.write("Hello 👋")
     #st.bar_chart(np.random.randn(30, 3))
